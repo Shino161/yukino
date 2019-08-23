dbc5c1672a8893c5945b910a0e8e1d7a122c2768
shino161 <shino161@outlook.com>
2019-08-23 14:28:41 +0800

    feat: 引入babel插件

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -10,7 +10,7 @@ module.exports = {
     rules: [
 			{
         test: /\.(js|jsx)$/,
-				exclude: /node_modules/, 
+        exclude: /(node_modules|bower_components)/, 
         loader: "babel-loader"
 			},
       {
--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
+import { Button } from 'antd';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1> hensin!卡面来大 </h1>
+        <div className="App">
+          <Button type="primary">Button</Button>
+        </div>
       </div>
+      
     );
   }
 }
