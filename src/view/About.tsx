f75463a06b395bd05f789d6b553e575183e07198
shino161 <shino161@outlook.com>
2019-09-12 14:42:00 +0800

    feat: demo page

--- src/view/About.tsx
+++ src/view/About.tsx
@@ -1,10 +1,14 @@
 
 import React from 'react';
-
+import { Link } from "react-router-dom";
 
 const Home = () => (
   <div>
-    2222
+    <p>This is a about page</p>
+    <p>这是一个 about 页面</p>
+    <p>
+      <Link to="/">Back to Home page</Link>
+    </p>
   </div>
 );
 export default Home;
--- src/view/Home.tsx
+++ src/view/Home.tsx
@@ -5,31 +5,28 @@ const ContentView = lazy(() => import('./ContentView'));
 const { Header, Content, Footer } = Layout;
 
 const Home = () => (
-      <div className="App">
-        <Layout className="layout" style={{ background: '#fff' }}>
-          <Header style={{ backgroundColor: '#fff', textAlign: 'left'}}>
-            <Menu
-              theme="light"
-              mode="horizontal"
-              defaultSelectedKeys={['2']}
-              style={{ lineHeight: '64px' }}
-            >
-              <Menu.Item key="1">
-                {/* <Link to="/">Home</Link> */}
-              </Menu.Item>
-              <Menu.Item key="2">统计数据2</Menu.Item>
-              <Menu.Item key="3">统计数据3</Menu.Item>
-            </Menu>
-          </Header>
-          <Content style={{ padding: '0 50px' }}>
-            <div style={{  padding: 24, minHeight: 280 }}>
-              <Suspense fallback={<Skeleton active />}>
-                <ContentView />
-              </Suspense>
-            </div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>Yukino Created by Shino161</Footer>
-        </Layout>
-      </div>
+  <div className="App">
+    <Layout className="layout" style={{ background: '#fff' }}>
+      <Header style={{ backgroundColor: '#fff', textAlign: 'left' }}>
+        <Menu
+          theme="light"
+          mode="horizontal"
+          defaultSelectedKeys={['1']}
+          style={{ lineHeight: '64px' }}
+        >
+          <Menu.Item key="1"><Link to="/">Home</Link></Menu.Item>
+          <Menu.Item key="2"><Link to="/about">About</Link></Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <div style={{ padding: 24, minHeight: 280 }}>
+          <Suspense fallback={<Skeleton active />}>
+            <ContentView />
+          </Suspense>
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Yukino Created by Shino161</Footer>
+    </Layout>
+  </div>
 );
 export default Home;
