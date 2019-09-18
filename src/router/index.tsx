4871299561f7728d8d089a9b4fdb15828011f1e5
shino161 <shino161@outlook.com>
2019-09-18 16:39:06 +0800

    feat: update demo page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -22,6 +22,4 @@ function RouteConfig() {
   );
 }
 
-console.log(RouteConfig);
-
 export default RouteConfig;
---
+++ src/view/CarouselComp.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Carousel} from 'antd';
+
+const CarouselComp = () => (
+  <div>
+    <Carousel autoplay >
+      <div>
+        <h3>3</h3>
+      </div>
+      <div>
+        <h3>3</h3>
+      </div>
+      <div>
+        <h3>4</h3>
+      </div>
+      <div>
+        <h3>5</h3>
+      </div>
+    </Carousel>
+  </div>
+);
+export default CarouselComp;
