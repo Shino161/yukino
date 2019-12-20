251913100840c83815a35354cea441f1ac247eb2
shino161 <shino161@outlook.com>
2019-12-20 14:14:50 +0800

    feat: add eslint-config-alloy

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,49 @@
-import { BrowserRouter as Router, Link } from 'react-router-dom';
-import React, { Suspense } from 'react';
-import { Spin, Layout, Menu } from 'antd';
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import React, { Suspense } from "react";
+import { Spin, Layout, Menu } from "antd";
 const { Header, Content, Footer } = Layout;
-import RouterCofig from './router/index'
-import './global.less'
+import RouterCofig from "./router/index";
+import "./global.less";
 const App = () => (
   <Router>
-    <Layout className="layout" style={{ background: '#fff' }}>
-      <Header style={{ backgroundColor: '#fff', textAlign: 'left' }}>
+    <Layout className="layout" style={{ background: "#fff" }}>
+      <Header style={{ backgroundColor: "#fff", textAlign: "left" }}>
         <Menu
           theme="light"
           mode="horizontal"
-          defaultSelectedKeys={['1']}
-          style={{ lineHeight: '64px'}}
+          defaultSelectedKeys={["1"]}
+          style={{ lineHeight: "64px" }}
         >
-          <Menu.Item key="1"><Link to="/">Home</Link></Menu.Item>
-          <Menu.Item key="2"><Link to="/about">ReactHooks</Link></Menu.Item>
+          <Menu.Item key="1">
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Link to="/about">ReactHooks</Link>
+          </Menu.Item>
         </Menu>
       </Header>
-      <Content style={{ padding: '0 50px' }}>
+      <Content style={{ padding: "0 50px" }}>
         <div style={{ padding: 24, minHeight: 280 }}>
-          <Suspense fallback={<Spin size="large" style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }} />}>
-            <RouterCofig></RouterCofig>
+          <Suspense
+            fallback={
+              <Spin
+                size="large"
+                style={{
+                  position: "fixed",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)"
+                }}
+              />
+            }
+          >
+            <RouterCofig />
           </Suspense>
         </div>
       </Content>
-      <Footer style={{ textAlign: 'center' }}>Yukino Created by Shino161</Footer>
+      <Footer style={{ textAlign: "center" }}>
+        Yukino Created by Shino161
+      </Footer>
     </Layout>
   </Router>
 );
--- src/view/About.tsx
+++ src/view/About.tsx
@@ -1,10 +1,9 @@
-
-import React, { useState, useEffect } from 'react';
-import { Button } from 'antd'
+import React, { useState, useEffect } from "react";
+import { Button } from "antd";
 import style from "./style.less";
 
 function Example() {
-  const [count, setCount] = useState(0); 
+  const [count, setCount] = useState(0);
   useEffect(() => {
     // 使用浏览器的 API 更新页面标题
     document.title = `You clicked ${count} times`;
@@ -12,9 +11,7 @@ function Example() {
   return (
     <div>
       <p className={style.title}>You clicked {count} times</p>
-      <Button onClick={() => setCount(count + 1)}>
-        Click me
-      </Button>
+      <Button onClick={() => setCount(count + 1)}>Click me</Button>
     </div>
   );
 }
