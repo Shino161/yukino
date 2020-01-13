ea937219e6aecd28ac4a2d1dde3e5adebeb14b6a
shino161 <shino161@outlook.com>
2020-01-13 10:17:27 +0800

    feat: remove image-webpack-loader; upgrade react router dom

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -95,32 +95,7 @@ module.exports = merge(common, {
                         }
                     }
                 ],
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    // 图片压缩
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            mozjpeg: {
-                                progressive: true,
-                                quality: 65,
-                            },
-                            optipng: {
-                                enabled: false,
-                            },
-                            pngquant: {
-                                quality: '65-90',
-                                speed: 4,
-                            },
-                            gifsicle: {
-                                interlaced: false,
-                            },
-                        },
-                    },
-                ],
-            },
+            }
         ],
     },
     plugins: [
