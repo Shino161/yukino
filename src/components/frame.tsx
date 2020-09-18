42cc85fc14dcc44d9f0b55be3b9edeab45b74986
shino161 <shino161@outlook.com>
2020-09-18 17:20:21 +0800

    feat: add wonderful button css

--- src/components/frame.tsx
+++ src/components/frame.tsx
@@ -12,7 +12,7 @@ const Frame = (props: any) => (
         style={{ lineHeight: '64px' }}
       >
         <Menu.Item key="1">
-          <Link to="/">Home</Link>
+          <Link to="/">Some Demo</Link>
         </Menu.Item>
         <Menu.Item key="2">
           <Link to="/about">ReactHooks</Link>
